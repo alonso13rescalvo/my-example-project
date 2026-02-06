45a0381e98eb29f6f0b1f7874e0a0f660e5452c5
alonso13rescalvo <a.rescalvo.2023@alumnos.urjc.es>
2026-02-06 12:37:20 +0100

    new develop method

---
+++ develop.py
@@ -0,0 +1,7 @@
+def develop():
+    print("Hello from my-example-project!")
+    print("esto es un ejemplo")
+    print("esto es un ejemplo de develop.py")
+
+if __name__ == "__main__":
+    develop()
